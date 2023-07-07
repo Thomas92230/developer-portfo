cfbb06fab2b3113cc175b1656b3962623b0b250e
Icham Boudehane <boudehaneicham@gmail.com>
2023-07-07 19:31:28 +0200

    new-css

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -17,40 +17,42 @@ function Skills() {
     }
 
     return (
-        <div id ="skills"className="skills">
+        <div id="skills" className="skills">
+          <div className="skills-body">
             <div className="line-styling">
-              <div className="style-circle" style={{backgroundColor: theme.primary}}></div>
-              <div className="style-circle" style={{backgroundColor: theme.primary}}></div>
-              <div className="style-line" style={{backgroundColor: theme.primary}}></div>
+              <div className="style-circle" style={{ backgroundColor: theme.primary }}></div>
+              <div className="style-circle" style={{ backgroundColor: theme.primary }}></div>
+              <div className="style-line" style={{ backgroundColor: theme.primary }}></div>
             </div>
-
+      
             <div className="skillsHeader">
-                <h2 style={{color: theme.primary}}>Compétences</h2>
+              <h2 style={{ color: theme.primary }}>Compétences</h2>
             </div>
+      
             <div className="skillsContainer">
-                <div className="skill--scroll">
-                    <Marquee 
-                        gradient={false} 
-                        speed={80} 
-                        pauseOnHover={true}
-                        pauseOnClick={true} 
-                        delay={0}
-                        play={true} 
-                        direction="left"
-                    >
-                        {skillsData.map((skill, id) => (
-                            <div className="skill--box" key={id} style={skillBoxStyle}>
-                                <img src={skillsImage(skill)} alt={skill} />
-                                <h3 style={{color: theme.tertiary}}>
-                                    {skill}
-                                </h3>
-                            </div>
-                        ))}
-                    </Marquee>
-                </div>
+              <div className="skill--scroll">
+                <Marquee
+                  gradient={false}
+                  speed={80}
+                  pauseOnHover={true}
+                  pauseOnClick={true}
+                  delay={0}
+                  play={true}
+                  direction="left"
+                >
+                  {skillsData.map((skill, id) => (
+                    <div className="skill--box" key={id} style={skillBoxStyle}>
+                      <img src={skillsImage(skill)} alt={skill} />
+                      <h3 style={{ color: theme.tertiary }}>{skill}</h3>
+                    </div>
+                  ))}
+                </Marquee>
+              </div>
             </div>
+          </div>
         </div>
-    )
-}
-
-export default Skills
+      );
+      
+    }
+    
+export default Skills;
